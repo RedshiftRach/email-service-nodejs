07f63bf8195176da438e7ed9fc2bfceb77736f16
Rachel Ajayi <rachel.ajayi@icloud.com>
2024-03-03 16:17:12 +0000

    added email function

--- handler.js
+++ handler.js
@@ -1,6 +1,27 @@
-'use strict';
+// top of file
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({ region: 'us-east-1' });
 
 module.exports.sendEmail = async (event) => {
+  // inside the `exports.handler.sendHello` function, before the `return` statement
+const params = {
+  Destination: {
+    ToAddresses: ['rachel.ajayi@icloud.com'], // This should be your email address
+  },
+  Message: {
+    Body: {
+      Text: {
+        Data: 'This is a message generated automatically from a Lambda function.',
+      },
+    },
+    Subject: {
+      Data: 'Hello from Lambda',
+    },
+  },
+  Source: 'rachel.ajayi@icloud.com', // This is the email listed in sender. Set it to your email for this practice
+};
+await ses.sendEmail(params).promise();
+// in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
   return {
     statusCode: 200,
     body: JSON.stringify(
