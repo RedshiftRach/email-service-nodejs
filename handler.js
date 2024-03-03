873ae466201914af509208702d685a93243c66c3
Rachel Ajayi <rachel.ajayi@icloud.com>
2024-03-03 16:58:24 +0000

    making Amazon SES more dynamic

--- handler.js
+++ handler.js
@@ -4,9 +4,24 @@ const ses = new AWS.SES({ region: 'us-east-1' });
 
 module.exports.sendEmail = async (event) => {
   // inside the `exports.handler.sendHello` function, before the `return` statement
-const params = {
+  const queryParams = event.queryStringParameters || {};
+  // select from the query parameters
+let { email, message, subject } = queryParams;
+
+if (!email) {
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: 'Email is required',
+    }),
+  };
+}
+message = message || 'This is a message generated automatically from a Lambda function.';
+subject = subject || 'Hello from Lambda';
+
+  const params = {
   Destination: {
-    ToAddresses: ['rachel.ajayi@icloud.com'], // This should be your email address
+    ToAddresses: [email], // This should be your email address
   },
   Message: {
     Body: {
@@ -18,21 +33,21 @@ const params = {
       Data: 'Hello from Lambda',
     },
   },
-  Source: 'rachel.ajayi@icloud.com', // This is the email listed in sender. Set it to your email for this practice
+  Source: [email], // This is the email listed in sender. Set it to your email for this practice
 };
 await ses.sendEmail(params).promise();
 // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+return {
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: `Email sent to ${email} with subject ${subject} and message ${message}`,
+      input: event,
+    },
+    null,
+    2
+  ),
+};
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
